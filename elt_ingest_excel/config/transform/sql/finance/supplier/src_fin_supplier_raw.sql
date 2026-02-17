b54411f5eb73bc0d55c6197334da0a18f25e1a10
Rakesh Patel <emailrak@gmail.com>
2026-02-17 21:34:05 +0000

    supplier: add date parsing, raw guard

--- elt_ingest_excel/config/transform/sql/finance/supplier/src_fin_supplier_raw.sql
+++ elt_ingest_excel/config/transform/sql/finance/supplier/src_fin_supplier_raw.sql
@@ -4,8 +4,26 @@ CREATE TABLE src_fin_supplier
     AS
   WITH cte_supplier_src
     AS (
-SELECT * 
-  FROM src_fin_supplier_raw      
+-- FA business unit
+SELECT 'FA'   business_unit, t.* FROM fin_supplier_creditor_created_date_fa            t
+UNION ALL
+SELECT 'FA'   business_unit, t.* FROM fin_supplier_creditor_last_payment_date_fa       t
+UNION ALL
+SELECT 'FA'   business_unit, t.* FROM fin_supplier_creditor_last_purchase_date_fa      t
+UNION ALL
+-- NFC business unit
+SELECT 'NFC'  business_unit, t.* FROM fin_supplier_creditor_created_date_nfc           t
+UNION ALL
+SELECT 'NFC'  business_unit, t.* FROM fin_supplier_creditor_last_payment_date_nfc      t
+UNION ALL
+SELECT 'NFC'  business_unit, t.* FROM fin_supplier_creditor_last_purchase_date_nfc     t
+UNION ALL
+-- WNSL business unit
+SELECT 'WNSL' business_unit, t.* FROM fin_supplier_creditor_created_date_wnsl          t
+UNION ALL
+SELECT 'WNSL' business_unit, t.* FROM fin_supplier_creditor_last_payment_date_wnsl     t
+UNION ALL
+SELECT 'WNSL' business_unit, t.* FROM fin_supplier_creditor_last_purchase_date_wnsl    t
        )
      , cte_supplier_distinct
     AS (
