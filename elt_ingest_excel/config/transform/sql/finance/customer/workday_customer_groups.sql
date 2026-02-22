30f00713609683e194292bad73db9ba2c94c72a6
Rakesh Patel <emailrak@gmail.com>
2026-02-22 17:12:33 +0000

    cleanup

--- elt_ingest_excel/config/transform/sql/finance/customer/workday_customer_groups.sql
+++ elt_ingest_excel/config/transform/sql/finance/customer/workday_customer_groups.sql
@@ -7,10 +7,10 @@ SELECT
      , c.nrm_customer_name                            customer_name
      , CASE
            WHEN UPPER(SUBSTR(TRIM(c.key_customer_name), 1, 1)) BETWEEN 'A' AND 'L'
-               THEN 'Customers_A_L'
+               THEN 'Customers_A_-_L'
            WHEN UPPER(SUBSTR(TRIM(c.key_customer_name), 1, 1)) BETWEEN 'M' AND 'Z'
-               THEN 'Customers_M_Z'
-           ELSE 'Customers_0_9'
+               THEN 'Customers_M_-_Z'
+           ELSE 'Customers_0_-_9'
        END                                            customer_group
   FROM src_fin_customer                c
 ;
