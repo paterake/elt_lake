4f3deb35ba66ce0ce810a5e79d6bdd514e3bbfe4
Rakesh Patel <emailrak@gmail.com>
2026-02-26 14:01:25 +0000

    initcaps on names

--- elt_ingest_excel/config/transform/sql/hcm/contingent_worker/src_hcm_contingent_worker.sql
+++ elt_ingest_excel/config/transform/sql/hcm/contingent_worker/src_hcm_contingent_worker.sql
@@ -4,11 +4,11 @@ CREATE TABLE src_hcm_contingent_worker
     AS
 SELECT
        t.*
-     , NULLIF(INITCAP(TRIM(t.first_name            )), '')                 nrm_first_name
-     , NULLIF(INITCAP(TRIM(t.last_name             )), '')                 nrm_last_name
+     , NULLIF(list_aggregate(list_transform(split(lower(TRIM(t.first_name            )), ' '), x -> upper(x[1]) || substr(x, 2)), 'string_agg', ' '), '') nrm_first_name
+     , NULLIF(list_aggregate(list_transform(split(lower(TRIM(t.last_name             )), ' '), x -> upper(x[1]) || substr(x, 2)), 'string_agg', ' '), '') nrm_last_name
      , NULLIF(LOWER(TRIM(t.primary_email           )), '')                 nrm_primary_email
      , NULLIF(LOWER(TRIM(t.secondary_email         )), '')                 nrm_secondary_email
-     , NULLIF(INITCAP(TRIM(t.manager_name          )), '')                 nrm_manager_name
+     , NULLIF(list_aggregate(list_transform(split(lower(TRIM(t.manager_name          )), ' '), x -> upper(x[1]) || substr(x, 2)), 'string_agg', ' '), '') nrm_manager_name
      , NULLIF(LOWER(TRIM(t.manager_email_address   )), '')                 nrm_manager_email_address
      , NULLIF(LOWER(TRIM(t.manager_email           )), '')                 nrm_manager_email
      , l.target_value                                                      nrm_location
--- elt_ingest_excel/config/transform/sql/hcm/contingent_worker/src_hcm_contingent_worker_id_mapping.sql
+++ elt_ingest_excel/config/transform/sql/hcm/contingent_worker/src_hcm_contingent_worker_id_mapping.sql
@@ -6,9 +6,9 @@ CREATE TABLE src_hcm_contingent_worker_id_mapping
     AS (
 SELECT DISTINCT
        w.employee_id                                                                   employee_id
-     , NULLIF(TRIM(INITCAP(w.legal_name_first_name )), '')                             first_name
-     , NULLIF(TRIM(INITCAP(w.legal_name_middle_name)), '')                             middle_name
-     , NULLIF(TRIM(INITCAP(w.legal_name_last_name  )), '')                             last_name
+     , NULLIF(list_aggregate(list_transform(split(lower(TRIM(w.legal_name_first_name )), ' '), x -> upper(x[1]) || substr(x, 2)), 'string_agg', ' '), '') first_name
+     , NULLIF(list_aggregate(list_transform(split(lower(TRIM(w.legal_name_middle_name)), ' '), x -> upper(x[1]) || substr(x, 2)), 'string_agg', ' '), '') middle_name
+     , NULLIF(list_aggregate(list_transform(split(lower(TRIM(w.legal_name_last_name  )), ' '), x -> upper(x[1]) || substr(x, 2)), 'string_agg', ' '), '') last_name
      , NULLIF(LOWER(TRIM(TRIM(u.email_address_raw, E'\r\n\t '))), '')                  email_address_raw
   FROM hcm_contingent_worker_id_mapping w
      , UNNEST(
