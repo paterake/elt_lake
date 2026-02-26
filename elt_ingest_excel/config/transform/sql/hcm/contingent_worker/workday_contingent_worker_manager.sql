e6aade491101368513359b25af48fedbf3062527
Rakesh Patel <emailrak@gmail.com>
2026-02-26 13:21:47 +0000

    set default line manager

--- elt_ingest_excel/config/transform/sql/hcm/contingent_worker/workday_contingent_worker_manager.sql
+++ elt_ingest_excel/config/transform/sql/hcm/contingent_worker/workday_contingent_worker_manager.sql
@@ -2,49 +2,60 @@ DROP TABLE IF EXISTS workday_contingent_worker_manager
 ;
 CREATE TABLE workday_contingent_worker_manager
     AS
-  WITH cte_match
+  WITH cte_manager_default
+    AS (
+SELECT m.employee_id                            employee_id
+     , m.first_name                             first_name
+     , m.last_name                              last_name
+  FROM src_hcm_contingent_worker_id_mapping     m
+ WHERE LOWER(m.first_name)                      = 'samuel'
+   AND LOWER(m.last_name)                       = 'vickers'
+       )
+     , cte_match
     AS (
 SELECT
-       t.contingent_worker_id                worker_id
-     , NULL                                  position_id
-     , m.employee_id                         worker_id_of_direct_manager
-     , m.first_name                          manager_first_name
-     , m.last_name                           manager_last_name
-     , t.nrm_first_name                      first_name
-     , t.nrm_last_name                       last_name
-     , 'Contingent Worker'                   worker_type
-     , NULL                                  manager_position_id
-     , NULL                                  supervisory_org_code
-     , NULL                                  supervisory_org_name
-     , NULL                                  supervisory_id
-     , NULL                                  staffing_model
+       t.contingent_worker_id                   worker_id
+     , NULL                                     position_id
+     , COALESCE(m.employee_id, md.employee_id)  worker_id_of_direct_manager
+     , COALESCE(m.first_name , md.first_name )  manager_first_name
+     , COALESCE(m.last_name  , md.last_name  )  manager_last_name
+     , t.nrm_first_name                         first_name
+     , t.nrm_last_name                          last_name
+     , 'Contingent Worker'                      worker_type
+     , NULL                                     manager_position_id
+     , NULL                                     supervisory_org_code
+     , NULL                                     supervisory_org_name
+     , NULL                                     supervisory_id
+     , NULL                                     staffing_model
      , CASE 
-        WHEN m.email_address                 = t.nrm_manager_email_address
-         AND m.manager_name                  = t.manager_name
+        WHEN m.email_address                    = t.nrm_manager_email_address
+         AND m.manager_name                     = t.nrm_manager_name
         THEN 1
-        WHEN m.email_address                 = t.nrm_manager_email
-         AND m.manager_name                  = t.manager_name
+        WHEN m.email_address                    = t.nrm_manager_email
+         AND m.manager_name                     = t.nrm_manager_name
         THEN 2
-        WHEN m.email_address                 = t.nrm_manager_email_address
+        WHEN m.email_address                    = t.nrm_manager_email_address
         THEN 3
-        WHEN m.email_address                 = t.nrm_manager_email
+        WHEN m.email_address                    = t.nrm_manager_email
         THEN 4
-        WHEN m.manager_name                  = t.nrm_manager_name
+        WHEN m.manager_name                     = t.nrm_manager_name
         THEN 5
         ELSE 999
-       END                                   match_priority
-  FROM src_hcm_contingent_worker             t
+       END                                      match_priority
+  FROM src_hcm_contingent_worker                t
+       CROSS JOIN 
+       cte_manager_default                      md
        LEFT OUTER JOIN 
-       src_hcm_contingent_worker_id_mapping  m
-          ON m.manager_name                  = t.nrm_manager_name
-          OR m.email_address                 = t.nrm_manager_email_address
-          OR m.email_address                 = t.nrm_manager_email
+       src_hcm_contingent_worker_id_mapping     m
+          ON m.manager_name                     = t.nrm_manager_name
+          OR m.email_address                    = t.nrm_manager_email_address
+          OR m.email_address                    = t.nrm_manager_email
        )
      , cte_match_rnk
     AS (
 SELECT t.*
      , ROW_NUMBER() OVER (PARTITION BY t.worker_id ORDER BY match_priority) rnk 
-  FROM cte_match                             t
+  FROM cte_match                                t
        )
 SELECT 
        t.worker_id
@@ -60,7 +71,7 @@ SELECT
      , t.supervisory_org_name
      , t.supervisory_id
      , t.staffing_model
-  FROM cte_match_rnk                         t
- WHERE t.rnk                                 = 1
-   AND t.worker_id_of_direct_manager         IS NOT NULL
+  FROM cte_match_rnk                            t
+ WHERE t.rnk                                    = 1
+   AND t.worker_id_of_direct_manager            IS NOT NULL
 ;
