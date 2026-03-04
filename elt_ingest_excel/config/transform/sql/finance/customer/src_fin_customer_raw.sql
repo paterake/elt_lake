442d7c870e11eebbede71d9be963d3e9f0e92089
Rakesh Patel <emailrak@gmail.com>
2026-03-04 15:03:27 +0000

    rework customer raw layer

--- elt_ingest_excel/config/transform/sql/finance/customer/src_fin_customer_raw.sql
+++ elt_ingest_excel/config/transform/sql/finance/customer/src_fin_customer_raw.sql
@@ -25,45 +25,51 @@ SELECT 'WNSL' business_unit, t.* FROM fin_customer_debtor_created_date_wnsl    t
 UNION ALL
 SELECT 'WNSL' business_unit, t.* FROM fin_customer_debtor_last_payment_date_wnsl t
        )
-     , cte_customer_time_nrm
+     , cte_customer_base
     AS (
 SELECT DISTINCT
        TRIM(t.customer_number)                                                                        nrm_customer_number
      , UPPER(COALESCE(NULLIF(UPPER(TRIM(t.customer_name)), ''), NULLIF(TRIM(t.customer_number), ''))) nrm_customer_name
+     , t.*
+  FROM cte_customer_src                      t
+       )
+     , cte_customer_nrm
+    AS (
+SELECT DISTINCT
+       UPPER(REGEXP_REPLACE(REGEXP_REPLACE(REPLACE(t.nrm_customer_name, '.', ''), '[^a-zA-Z0-9\s]', '', 'g'), '(\s|\b)(LTD|LIMITED|PLC|GROUP|HOLDINGS|FC|FOOTBALL|CLUB|ASSOCIATION)\b', '', 'gi')) 
+       clean_name
      , COALESCE(
           TRY_STRPTIME(NULLIF(TRIM(t.created_date          ), ''), '%Y-%m-%d %H:%M:%S')
         , TRY_STRPTIME(NULLIF(TRIM(t.created_date          ), ''), '%Y-%m-%d')
         , TRY_STRPTIME(NULLIF(TRIM(t.created_date          ), ''), '%d-%m-%Y')
-       )                                                                                              created_ts
+       )                                                                                              nrm_created_date
      , COALESCE(
           TRY_STRPTIME(NULLIF(TRIM(t.last_payment_date     ), ''), '%Y-%m-%d %H:%M:%S')
         , TRY_STRPTIME(NULLIF(TRIM(t.last_payment_date     ), ''), '%Y-%m-%d')
         , TRY_STRPTIME(NULLIF(TRIM(t.last_payment_date     ), ''), '%d-%m-%Y')
-       )                                                                                              last_payment_ts
+       )                                                                                              nrm_last_payment_date
      , COALESCE(
           TRY_STRPTIME(NULLIF(TRIM(t.last_transaction_date ), ''), '%Y-%m-%d %H:%M:%S')
         , TRY_STRPTIME(NULLIF(TRIM(t.last_transaction_date ), ''), '%Y-%m-%d')
         , TRY_STRPTIME(NULLIF(TRIM(t.last_transaction_date ), ''), '%d-%m-%Y')
-       )                                                                                              last_transaction_ts
-     , mbu.target_value                                                                               target_business_unit
+       )                                                                                              nrm_last_transaction_date
+     , mbu.target_value                                                                               nrm_business_unit
      , t.*
-  FROM cte_customer_src                      t
+  FROM cte_customer_base                     t
        INNER JOIN
        ref_source_business_unit_mapping      mbu
           ON UPPER(mbu.source_value)         = UPPER(TRIM(t.business_unit))
        ) 
-     , cte_customer_business_unit
+     , cte_customer_name_agg
     AS (
 SELECT t.nrm_customer_name
-     , ARRAY_AGG (DISTINCT t.business_unit              ORDER BY t.business_unit       ) array_business_unit
-     , ARRAY_AGG (DISTINCT t.target_business_unit       ORDER BY t.target_business_unit) array_target_business_unit
-     , STRING_AGG(DISTINCT t.business_unit        , '|' ORDER BY t.business_unit       ) pipe_business_unit
-     , STRING_AGG(DISTINCT t.target_business_unit , '|' ORDER BY t.target_business_unit) pipe_target_business_unit
-  FROM cte_customer_time_nrm           t
+     , ARRAY_AGG (DISTINCT t.nrm_business_unit      ORDER BY t.nrm_business_unit) array_nrm_business_unit
+     , STRING_AGG(DISTINCT t.nrm_business_unit, '|' ORDER BY t.nrm_business_unit) pipe_nrm_business_unit
+  FROM cte_customer_nrm                      t
  GROUP BY 
        t.nrm_customer_name
        )
-     , cte_customer
+     , cte_customer_rank
     AS (
 SELECT 
        COUNT() OVER (PARTITION BY t.nrm_customer_number)                               nrm_customer_id_count
@@ -73,21 +79,19 @@ SELECT
        (
          PARTITION BY t.nrm_customer_name
              ORDER BY
-                   t.created_ts          ASC  NULLS LAST
-                 , t.last_payment_ts     DESC NULLS LAST
-                 , t.last_transaction_ts DESC NULLS LAST
+                   t.nrm_created_date           ASC  NULLS LAST
+                 , t.nrm_last_payment_date      DESC NULLS LAST
+                 , t.nrm_last_transaction_date  DESC NULLS LAST
        )                                                                         data_rnk
      , t.* 
-     , bu.array_business_unit
-     , bu.array_target_business_unit
-     , bu.pipe_business_unit
-     , bu.pipe_target_business_unit
-  FROM cte_customer_time_nrm           t
+     , agg.array_nrm_business_unit
+     , agg.pipe_nrm_business_unit
+  FROM cte_customer_nrm                t
        INNER JOIN
-       cte_customer_business_unit      bu
-         ON bu.nrm_customer_name       = t.nrm_customer_name
+       cte_customer_name_agg           agg
+         ON agg.nrm_customer_name      = t.nrm_customer_name
        )
 SELECT 
        t.*
-  FROM cte_customer                    t
+  FROM cte_customer_rank               t
 ;
--- elt_ingest_excel/config/transform/sql/finance/customer/workday_customer_company_restrictions.sql
+++ elt_ingest_excel/config/transform/sql/finance/customer/workday_customer_company_restrictions.sql
@@ -5,7 +5,7 @@ CREATE TABLE workday_customer_company_restrictions
 SELECT
        c.customer_id                   customer_id
      , c.nrm_customer_name             customer_name
-     , c.pipe_target_business_unit     restricted_to_companies
+     , c.pipe_nrm_business_unit        restricted_to_companies
   FROM 
        src_fin_customer                                  c
 ;
