bd584e2011f5f3335bfad492b85ef40e559adc9b
Rakesh Patel <emailrak@gmail.com>
2026-03-09 14:31:16 +0000

    change to new files

--- elt_ingest_excel/config/transform/sql/finance/customer/src_fin_customer_raw.sql
+++ elt_ingest_excel/config/transform/sql/finance/customer/src_fin_customer_raw.sql
@@ -9,26 +9,9 @@ CREATE TABLE src_fin_customer_raw
     AS
   WITH cte_customer_src
     AS (
--- FA business unit
-SELECT 'FA'   business_unit, t.* FROM fin_customer_debtor_last_3_years_fa      t
+SELECT t.company business_unit, t.* FROM fin_customer_debtor_no_activity_1_year      t
 UNION ALL
-SELECT 'FA'   business_unit, t.* FROM fin_customer_debtor_created_date_fa      t
-UNION ALL
-SELECT 'FA'   business_unit, t.* FROM fin_customer_debtor_last_payment_date_fa t
-UNION ALL
--- NFC business unit
-SELECT 'NFC'  business_unit, t.* FROM fin_customer_debtor_last_3_years_nfc     t
-UNION ALL
-SELECT 'NFC'  business_unit, t.* FROM fin_customer_debtor_created_date_nfc     t
-UNION ALL
-SELECT 'NFC'  business_unit, t.* FROM fin_customer_debtor_last_payment_date_nfc t
-UNION ALL
--- WNSL business unit
-SELECT 'WNSL' business_unit, t.* FROM fin_customer_debtor_last_3_years_wnsl    t
-UNION ALL
-SELECT 'WNSL' business_unit, t.* FROM fin_customer_debtor_created_date_wnsl    t
-UNION ALL
-SELECT 'WNSL' business_unit, t.* FROM fin_customer_debtor_last_payment_date_wnsl t
+SELECT t.company business_unit, t.* FROM fin_customer_debtor_activity_2_years        t
        )
      , cte_customer_base
     AS (
--- elt_ingest_excel/config/transform/sql/finance/supplier/src_fin_supplier_raw.sql
+++ elt_ingest_excel/config/transform/sql/finance/supplier/src_fin_supplier_raw.sql
@@ -9,26 +9,9 @@ CREATE TABLE src_fin_supplier_raw
     AS
   WITH cte_supplier_src
     AS (
--- FA business unit
-SELECT 'FA'   business_unit, t.* FROM fin_supplier_creditor_created_date_fa            t
+SELECT t.company business_unit, t.* FROM fin_supplier_creditor_no_activity_1_year      t
 UNION ALL
-SELECT 'FA'   business_unit, t.* FROM fin_supplier_creditor_last_payment_date_fa       t
-UNION ALL
-SELECT 'FA'   business_unit, t.* FROM fin_supplier_creditor_last_purchase_date_fa      t
-UNION ALL
--- NFC business unit
-SELECT 'NFC'  business_unit, t.* FROM fin_supplier_creditor_created_date_nfc           t
-UNION ALL
-SELECT 'NFC'  business_unit, t.* FROM fin_supplier_creditor_last_payment_date_nfc      t
-UNION ALL
-SELECT 'NFC'  business_unit, t.* FROM fin_supplier_creditor_last_purchase_date_nfc     t
-UNION ALL
--- WNSL business unit
-SELECT 'WNSL' business_unit, t.* FROM fin_supplier_creditor_created_date_wnsl          t
-UNION ALL
-SELECT 'WNSL' business_unit, t.* FROM fin_supplier_creditor_last_payment_date_wnsl     t
-UNION ALL
-SELECT 'WNSL' business_unit, t.* FROM fin_supplier_creditor_last_purchase_date_wnsl    t
+SELECT t.company business_unit, t.* FROM fin_supplier_creditor_activity_2_years        t
        )
      , cte_supplier_base
     AS (
