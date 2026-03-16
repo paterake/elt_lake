1177d8ce8aeedcce5a55d293abd5f0873cf3cf85
Rakesh Patel <emailrak@gmail.com>
2026-03-16 23:32:56 +0000

    payment types cleanup

--- elt_ingest_excel/config/transform/sql/finance/supplier/workday_supplier_payment.sql
+++ elt_ingest_excel/config/transform/sql/finance/supplier/workday_supplier_payment.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS workday_supplier_payment
 ;
 CREATE TABLE workday_supplier_payment 
     AS
+  WITH cte_supplier_paymment
+    AS (
 SELECT
        t.supplier_id                                     supplier_id
      , t.nrm_supplier_name                               supplier_name
@@ -13,75 +15,34 @@ SELECT
           AND t.nrm_bank_account_number   IS NOT NULL
          THEN 'BACS|Manual'
          WHEN t.nrm_currency_code         = 'GBP'
-          AND t.nrm_bank_iban             IS NOT NULL
+          AND t.nrm_bank_iban             IS NOT NULL          
          THEN 'Wire|Manual'
          WHEN t.nrm_currency_code         = 'EUR'
           AND t.nrm_bank_iban             IS NOT NULL
          THEN 'SEPA|Wire|Manual'
          WHEN t.nrm_currency_code         = 'EUR'
-          AND t.nrm_bank_name             IS NOT NULL
-          AND t.nrm_bank_sort_code        IS NOT NULL
-          AND t.nrm_bank_account_number   IS NOT NULL
-         THEN 'Wire|Manual'
-         WHEN t.nrm_currency_code         = 'USD'
-          AND t.nrm_bank_iban             IS NOT NULL
-         THEN 'Wire|Manual'
-         WHEN t.nrm_currency_code         = 'USD'
           AND t.nrm_bank_account_number   IS NOT NULL
           AND t.nrm_bank_swift_code       IS NOT NULL
          THEN 'Wire|Manual'
-         WHEN t.nrm_currency_code         = 'USD'
+         WHEN t.nrm_currency_code         = 'EUR'
           AND t.nrm_bank_name             IS NOT NULL
           AND t.nrm_bank_sort_code        IS NOT NULL
           AND t.nrm_bank_account_number   IS NOT NULL
          THEN 'Wire|Manual'
-         WHEN t.nrm_currency_code         IN ('CHF','AUD','PLN','SEK','AED','QAR','DKK','NOK')
+         WHEN t.nrm_currency_code         IN ('USD', 'CHF','AUD','PLN','SEK','AED','QAR','DKK','NOK')
+          AND t.nrm_bank_iban             IS NOT NULL
          THEN 'Wire|Manual'
-         WHEN t.nrm_currency_code         IN ('CHF','AUD','PLN','SEK','AED','QAR','DKK','NOK')
+         WHEN t.nrm_currency_code         IN ('USD', 'CHF','AUD','PLN','SEK','AED','QAR','DKK','NOK')
+          AND t.nrm_bank_account_number   IS NOT NULL
+          AND t.nrm_bank_swift_code       IS NOT NULL
+         THEN 'Wire|Manual'
+         WHEN t.nrm_currency_code         IN ('USD', 'CHF','AUD','PLN','SEK','AED','QAR','DKK','NOK')
           AND t.nrm_bank_name             IS NOT NULL
           AND t.nrm_bank_sort_code        IS NOT NULL
           AND t.nrm_bank_account_number   IS NOT NULL
          THEN 'Wire|Manual'
          ELSE 'Manual'
-       END                                              payment_types_accepted
-     , CASE
-         WHEN t.nrm_currency_code         = 'GBP'
-          AND t.nrm_bank_name             IS NOT NULL
-          AND t.nrm_bank_sort_code        IS NOT NULL
-          AND t.nrm_bank_account_number   IS NOT NULL
-         THEN 'BACS'
-         WHEN t.nrm_currency_code         = 'GBP'
-          AND t.nrm_bank_iban             IS NOT NULL
-         THEN 'Wire'
-         WHEN t.nrm_currency_code         = 'EUR'
-          AND t.nrm_bank_iban             IS NOT NULL
-         THEN 'SEPA'
-         WHEN t.nrm_currency_code         = 'EUR'
-          AND t.nrm_bank_name             IS NOT NULL
-          AND t.nrm_bank_sort_code        IS NOT NULL
-          AND t.nrm_bank_account_number   IS NOT NULL
-         THEN 'Wire'
-         WHEN t.nrm_currency_code         = 'USD'
-          AND t.nrm_bank_iban             IS NOT NULL
-         THEN 'Wire'
-         WHEN t.nrm_currency_code         = 'USD'
-          AND t.nrm_bank_account_number   IS NOT NULL
-          AND t.nrm_bank_swift_code       IS NOT NULL
-         THEN 'Wire'
-         WHEN t.nrm_currency_code         = 'USD'
-          AND t.nrm_bank_name             IS NOT NULL
-          AND t.nrm_bank_sort_code        IS NOT NULL
-          AND t.nrm_bank_account_number   IS NOT NULL
-         THEN 'Wire'
-         WHEN t.nrm_currency_code         IN ('CHF','AUD','PLN','SEK','AED','QAR','DKK','NOK')
-         THEN 'Wire'
-         WHEN t.nrm_currency_code         IN ('CHF','AUD','PLN','SEK','AED','QAR','DKK','NOK')
-          AND t.nrm_bank_name             IS NOT NULL
-          AND t.nrm_bank_sort_code        IS NOT NULL
-          AND t.nrm_bank_account_number   IS NOT NULL
-         THEN 'Wire'
-         ELSE 'Manual'
-       END                                              default_payment_type
+       END                                               payment_types_accepted
      , NULL                                              shipping_terms
      , NULL                                              always_separate_payments
      , NULL                                              do_not_pay_during_bank_account_updates
@@ -90,4 +51,17 @@ SELECT
      , NULL                                              exclude_tax_amount_from_supplier_invoice_discount
   FROM src_fin_supplier                t
  WHERE t.nrm_payment_terms_id          IS NOT NULL
+       )
+SELECT t.supplier_id
+     , t.supplier_name
+     , t.payment_terms
+     , t.payment_types_accepted
+     , SPLIT_PART(t.payment_types_accepted, '|', 1)      default_payment_type
+     , t.shipping_terms
+     , t.always_separate_payments
+     , t.do_not_pay_during_bank_account_updates
+     , t.exclude_freight_amount_from_supplier_invoice_discount
+     , t.exclude_other_charge_from_supplier_invoice_discount
+     , t.exclude_tax_amount_from_supplier_invoice_discount
+  FROM cte_supplier_paymment           t
 ;
