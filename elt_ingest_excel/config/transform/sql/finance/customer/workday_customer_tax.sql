292628c9dafa0d3c6765966ec06fa88cf5ee494d
Rakesh Patel <emailrak@gmail.com>
2026-03-16 20:31:40 +0000

    tidy

--- elt_ingest_excel/config/transform/sql/finance/customer/workday_customer_tax.sql
+++ elt_ingest_excel/config/transform/sql/finance/customer/workday_customer_tax.sql
@@ -3,15 +3,15 @@ DROP TABLE IF EXISTS workday_customer_tax
 CREATE TABLE workday_customer_tax
     AS
 SELECT
-       c.customer_id                                                            customer_id
-     , c.nrm_customer_name                                                      customer_name
-     , 'GBR 20% VAT (20%)'                                                      tax_code
-     , fn_nvl2(c.nrm_tax_registration_number, NULL, c.nrm_country_name)         tax_id_country
-     , c.nrm_tax_registration_number                                            tax_id
-     , fn_nvl2(c.nrm_tax_registration_number, NULL, c.nrm_tax_id_type)         tax_id_type
-     , fn_nvl2(c.nrm_tax_registration_number, NULL, 'Yes')                     primary_tax_id
-     , fn_nvl2(c.nrm_tax_registration_number, NULL, 'Yes')                     transaction_tax_id
-     , c.nrm_country_name                                                       tax_status_country
+       c.customer_id                                                             customer_id
+     , c.nrm_customer_name                                                       customer_name
+     , 'GBR 20% VAT (20%)'                                                       tax_code
+     , fn_nvl2(c.nrm_tax_registration_number, NULL, c.nrm_country_name)          tax_id_country
+     , c.nrm_tax_registration_number                                             tax_id
+     , fn_nvl2(c.nrm_tax_registration_number, NULL, c.nrm_tax_id_type)           tax_id_type
+     , fn_nvl2(c.nrm_tax_registration_number, NULL, 'Yes')                       primary_tax_id
+     , fn_nvl2(c.nrm_tax_registration_number, NULL, 'Yes')                       transaction_tax_id
+     , c.nrm_country_name                                                        tax_status_country
      , CASE
          WHEN UPPER(c.nrm_country_code) = 'GB'
          THEN CASE
