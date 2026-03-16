63a7c5a659815797710d7b27cc6992811ac55940
Rakesh Patel <emailrak@gmail.com>
2026-03-16 18:18:35 +0000

    tax population

--- elt_ingest_excel/config/transform/sql/finance/supplier/workday_supplier_tax.sql
+++ elt_ingest_excel/config/transform/sql/finance/supplier/workday_supplier_tax.sql
@@ -30,16 +30,10 @@ SELECT
          WHEN UPPER(t.nrm_country_code) = 'GB'
          THEN CASE
                 WHEN t.nrm_tax_schedule_id = 'POS'
-                THEN CASE
-                       WHEN t.nrm_tax_id_number IS NOT NULL
-                       THEN 'Supplier of Services - VAT/GST Registered in this Country'
-                       ELSE 'Supplier of Services - Not VAT/GST Registered in this Country'
-                     END
+                THEN 'Supplier of Services - VAT/GST Registered in this Country'
                 WHEN t.nrm_tax_schedule_id IN ('PZ','PX')
                 THEN 'Supplier of VAT/GST Exempt Goods'
-                WHEN t.nrm_tax_id_number IS NOT NULL
-                THEN 'Supplier of Goods - VAT/GST Registered in this Country'
-                ELSE 'Supplier of Goods - Not VAT/GST Registered in this Country'
+                ELSE 'Supplier of Goods - VAT/GST Registered in this Country'
               END
          WHEN UPPER(t.nrm_country_code) IN (
                 'AT','BE','BG','HR','CY','CZ','DE','DK','EE','ES','FI','FR'
@@ -48,19 +42,13 @@ SELECT
               )
          THEN CASE
                 WHEN t.nrm_tax_schedule_id = 'POS'
-                THEN 'EU Supplier of Services - Not VAT Registered in this Country'
-                ELSE 'EU Supplier of Goods - Not VAT Registered in this Country'
+                THEN 'Supplier of Services – Buyer to Reverse Charge VAT/GST'
+                ELSE 'Supplier of Goods – Buyer to Reverse Charge VAT/GST'
               END
          ELSE CASE
                 WHEN t.nrm_tax_schedule_id = 'POS'
-                THEN CASE
-                       WHEN t.nrm_tax_id_number IS NOT NULL
-                       THEN 'Supplier of Services - VAT/GST Registered in this Country'
-                       ELSE 'Supplier of Services - Not VAT/GST Registered in this Country'
-                     END
-                WHEN t.nrm_tax_id_number IS NOT NULL
-                THEN 'Supplier of Goods - VAT/GST Registered in this Country'
-                ELSE 'Supplier of Goods - Not VAT/GST Registered in this Country'
+                THEN 'Supplier of Services - VAT/GST Registered in this Country'
+                ELSE 'Supplier of Goods - VAT/GST Registered in this Country'
               END
        END                                                              transaction_tax_status
      , NULL                                                             withholding_tax_status
