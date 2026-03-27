71e81569573e26f1f3f39f2336af4cceea7b5512
Rakesh Patel <emailrak@gmail.com>
2026-03-27 10:25:36 +0000

    add more validation checks

--- elt_ingest_excel/config/transform/sql/finance/supplier/validation_log.sql
+++ elt_ingest_excel/config/transform/sql/finance/supplier/validation_log.sql
@@ -1,9 +1,8 @@
 -- Ensure all entries have a primry defined
 SELECT * FROM workday_supplier_email WHERE supplier_id NOT IN (SELECT supplier_id FROM workday_supplier_email WHERE primary_flag = 'Yes');
-
--- Ensure all entries have a primry defined
 SELECT * FROM workday_supplier_phone WHERE supplier_id NOT IN (SELECT supplier_id FROM workday_supplier_phone WHERE primary_flag = 'Yes');
-
-
--- Ensure all entries have a primry defined
 SELECT * FROM workday_customer_email WHERE customer_id NOT IN (SELECT customer_id FROM workday_customer_email WHERE primary_flag = 'Yes');
+
+-- Validate Region population
+SELECT * FROM workday_supplier_address WHERE (country,region) NOT IN (SELECT country, instance FROM ref_workday_country_state_region);
+SELECT * FROM workday_customer_address WHERE (country,region) NOT IN (SELECT country, instance FROM ref_workday_country_state_region);
